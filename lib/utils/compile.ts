89a9e5f0a45bba18b216e959d9859d717eb60ac6
Haneen Mahdin <haneenmahdin@gmail.com>
2021-05-16 22:40:43 +0530

    `feature`: add build file support

--- lib/utils/compile.ts
+++ lib/utils/compile.ts
@@ -1,8 +1,16 @@
+import ts from 'typescript';
+import fs from 'fs';
+import path from 'path';
 import transpile from './transpile';
 import readFile from './readFile';
-import ts from 'typescript';
 
-function compile(file) {
+/**
+ * @param {string} file Path to the file
+ * @param {boolean} shouldBuild whether to result output the file
+ * @param {string} buildFileName File Name of the build file
+ * @returns {void} Compiles and runs the code
+ */
+function compile(file: string, shouldBuild?: boolean, buildFileName?: string): void {
     const data = readFile(file);
 
     let jsCode = transpile(data, {
@@ -10,6 +18,15 @@ function compile(file) {
             module: ts.ModuleKind.ES2015
         }
     });
+
+    if (shouldBuild) {
+        const pathToBuild = path.join(path.dirname(file), buildFileName || path.basename(file).replace(".ts", ".js"));
+
+        fs.writeFileSync(pathToBuild, jsCode, {
+            encoding: 'utf-8'
+        });
+    }
+
 
     jsCode = jsCode.replace(/export\D+/g, '');
 
@@ -19,5 +36,3 @@ function compile(file) {
 }
 
 export default compile;
-
-compile(process.argv[2])
